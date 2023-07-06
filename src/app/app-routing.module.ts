60fb44933932f75afe69ce51d7c281f9084ffdda
Chabiev <95481264+Chabiev@users.noreply.github.com>
2023-07-06 15:52:27 +0400

    added login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import { HomeComponent } from './home/home.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomeComponent},
+  {path: 'Login', component: LoginFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
